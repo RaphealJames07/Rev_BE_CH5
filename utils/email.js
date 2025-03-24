0f7c501eb611b61e927974c64bd9b68bada3bff4
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2025-03-24 15:19:44 +0100

    signup

--- utils/email.js
+++ utils/email.js
@@ -1,30 +1,4 @@
-// const nodemailer = require('nodemailer');
 
-// const sendEmail = async (options) => {
-//   // 1) Create a transorter
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD,
-//     },
-//     // Activate in gmail "less secure app" option
-//   });
-
-//   // 2) Define the email options
-//   const mailOptions = {
-//     from: 'Rapheal James <testing@james.io>',
-//     to: options.email,
-//     subject: options.subject,
-//     text: options.message,
-//     //html:
-//   };
-
-//   // 3) Send the email
-//   await transporter.sendMail(mailOptions);
-// };
-// module.exports = sendEmail;
 const dotenv = require("dotenv");
 
 dotenv.config({path: "./config.env"});
